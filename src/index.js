8424152fdcbfc54ce56a8f516dac472ba855ffc2
Jonathan <u605592@teco.com.ar>
2022-03-17 17:22:48 -0300

    se agrega boostrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './styles/index.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import "./../node_modules/bootstrap/dist/css/bootstrap.css"
 
 ReactDOM.render(
   <React.StrictMode>
