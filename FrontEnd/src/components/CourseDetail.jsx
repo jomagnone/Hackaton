1509e868a808485cd2a757853501250bae595e20
Jonathan <u605592@teco.com.ar>
2022-03-18 12:41:04 -0300

    se agregan apis

--- FrontEnd/src/components/CourseDetail.jsx
+++ FrontEnd/src/components/CourseDetail.jsx
@@ -1,11 +1,10 @@
 import '../styles/CourseDetail.css';
 import Button from 'react-bootstrap/Button'
-import {useEffect, useState,useContext} from 'react';
 import {Link} from 'react-router-dom';
 
 function CourseDetail ({data})  {
 
-  
+
     return (
         <div className="fila">
             <div className="ImgGallery columnGalerry">
@@ -13,10 +12,11 @@ function CourseDetail ({data})  {
             </div>  
             <div className="columnGalerry" >
                 <div className="title" >{data.titulo}</div>
-                <div className="description" >{data.description}</div>
+                <div className="description" >{data.descripcion}</div>
                 <div className="stock" ><b>Stock: </b>{data.stock}</div>
+                <div className="btnCourseDetail" > 
                 <Link to="/myLearning"> <Button variant="success">Anotarse</Button>{''} </Link> 
-                <br />
+                </div>
                 
               
             </div>  
--- FrontEnd/src/pages/DetailCourse.jsx
+++ FrontEnd/src/pages/DetailCourse.jsx
@@ -5,15 +5,28 @@ import CircularProgress from '@mui/material/CircularProgress';
 import { useParams} from 'react-router-dom';
 
 import datos from '../data/courses.jsx';
+import {FetchCourse} from '../utils/Fetch';
 
 
 const  DetailCourse = () => {
-
+  
+  
   const {idCourse} = useParams();
+  const [curso, setCurso] = useState({});
+
+  useEffect(() => {
+
+    FetchCourse(idCourse)
+          .then( result => {setCurso(result[0]);})
+          .catch(error => console.log(error)) ; 
+
+    }, []);
+
+
     return (
       <>
         <div className="DetailPoroductsContainter" >
-        {(datos) ? <CourseDetail data = {datos[idCourse]} />
+        {(curso) ? <CourseDetail data = {curso}/>
         : <div className="fila progres">  <CircularProgress color="primary" /> </div>
          }
 
